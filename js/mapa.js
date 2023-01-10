e1315d45523aea99964f86fc5dc8894f74750836
kenkairon <ingeinfo2006@gmail.com>
2023-01-10 18:35:46 -0300

    Refinamiento de código

---
+++ js/mapa.js
@@ -0,0 +1,85 @@
+
+const regionesDeChile = [
+    {
+    nombre: 'Arica y Parinacota',
+    atractivos: 'Parque Nacional Lauca',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Parque_Nacional_Lauca_-_panoramio.jpg'
+    },
+    {
+    nombre: 'Tarapacá',
+    atractivos: 'Oasis de Pica',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/5/58/Oasis_de_Pica._I_regi%C3%B3n..jpg'
+    },
+    {
+    nombre: 'Antofagasta',
+    atractivos: 'El Valle de la luna',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/4/46/El_Valle_de_la_Luna_16.jpg'
+    },
+    {
+    nombre: 'Atacama',
+    atractivos: 'Laguna Verde',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Laguna_Verde_Bolivia.jpg'
+    },
+    {
+    nombre: 'Coquimbo',
+    atractivos: 'Playa la Herradura',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Playa_La_Herradura%2C_Coquimbo%2C_Regi%C3%B3n_de_Coquimbo.jpg'
+    },
+    {
+    nombre: 'Valparaíso',
+    atractivos: 'Cerro Alegre',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/d/da/Cerro_Alegre_January_2012_05.jpg'
+    },
+    {
+    nombre:  'Metropolitana de Santiago',
+    atractivos: 'Metropolitana de Santiago',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Caj%C3%B3n_Del_Maipo%2C_Regi%C3%B3n_Metropolitana%2C_Chile_-_panoramio.jpg'
+    },
+    {
+    nombre:  'Libertador General Bernardo O\'Higgins',
+    atractivos: 'Parque Safari Chile.',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/0/02/SP_-_Parque_Estadual_das_Fontes_do_Ipiranga_-_Zoo_Saf%C3%A1ri_-_Zoo_Saf%C3%A1ri_-_Lhama.jpg'
+    },
+    {
+    nombre: 'Maule',
+    atractivos: 'Lago Cobún',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/6/67/Lago_Colbun_-_panoramio.jpg'
+    },
+    {
+    nombre: 'Ñuble',
+    atractivos: 'Reserva Nacional Ñuble',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Mapa_de_Reserva_Nacional_Los_Huemules_de_Niblinto.png'
+    },
+    {
+    nombre: 'Biobío',
+    atractivos: 'Conocer el segundo Chile',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Chenqueco.JPG/480px-Chenqueco.JPG'
+    },
+    {
+    nombre: 'La Araucanía',
+    atractivos: 'Que quede la Zorra en el Campo',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Araucaria_araucana_-_Parque_Nacional_Conguill%C3%ADo_por_lautaroj_-_001.jpg/480px-Araucaria_araucana_-_Parque_Nacional_Conguill%C3%ADo_por_lautaroj_-_001.jpg'
+    },
+    {
+    nombre: 'Los Ríos',
+    atractivos: 'Agüita por todos lados',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Chenqueco.JPG/480px-Chenqueco.JPG'
+    },
+    {
+    nombre: 'Los Lagos',
+    atractivos: 'Mucha más Agua que en los Ríos',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Catedral_de_Osorno%2C_Osorno%2C_Chile1.jpg/480px-Catedral_de_Osorno%2C_Osorno%2C_Chile1.jpg'
+    },
+    {
+    nombre:  'Aysén del General Carlos Ibáñez del Campo',
+    atractivos: 'Pastar como Vaca',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/.00_3210_Puerto_Chacabuco_-_Chile.jpg/480px-.00_3210_Puerto_Chacabuco_-_Chile.jpg'
+    },
+    {
+    nonbre: 'Magallanes',
+    atractivos: 'Estar fuera de Chile',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Glaciar_Serrano%2C_Laguna_Serrano%2C_a_4_horas_de_Puerto_Natales%2C_Magallanes%2C_sur_de_Chile.jpg/480px-Glaciar_Serrano%2C_Laguna_Serrano%2C_a_4_horas_de_Puerto_Natales%2C_Magallanes%2C_sur_de_Chile.jpg'
+    },
+];
+
+
